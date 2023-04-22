c39c47ae086a30b05c0e2624770553a2adba9bd5
teguh ardama <teguhardama75@gmail.com>
2023-04-22 14:01:16 +0700

    integration landing page, checkout, my dashboard

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Checkout;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function dashboard()
+    {
+        switch (Auth::user()->is_admin) {
+            case true:
+                return redirect(route('admin.dashboard'));
+                break;
+            
+            default:
+                return redirect(route('user.dashboard'));
+                break;
+        }
+    }
+}
